8d19056443e0a9d57999cf367740b1c14b1977b6
santiagocalvoazumo <santiago.calvo@azumo.co>
2023-11-13 19:03:32 -0300

    Added styles to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import Image from "next/image";
-import "@/styles/global.css";
 import baseStyles from "@/styles/home.module.css";
+import Link from "next/link";
 
 const Index = () => {
   return (
     <div className={baseStyles["main-container"]}>
-      <div className={baseStyles["title-container"]}>
+      <div className={baseStyles["content-container"]}>
         <h1 className={baseStyles["title"]}>MTG Assistant</h1>
         <p className={baseStyles["subtitle"]}>
-          A Magic: The Gathering assistant to create decks, ask questions, and
-          more!
+          Talk to your own MTG Assistant to help you build your deck, suggest
+          cards, and more!
         </p>
         <div className={baseStyles["image-container"]}>
-          <Image
-            src="/mtgCardBack.jpg"
-            alt="MTG Card Back"
-            width={200}
-            height={280}
-            layout="responsive"
-          />
+          <Link href="/assistant">
+            <Image
+              src="/mtgCardBack.jpg"
+              alt="MTG Card Back"
+              width={400} // Adjusted size
+              height={560} // Adjusted size to maintain aspect ratio
+              className={baseStyles["image"]}
+              layout="intrinsic"
+            />
+          </Link>
         </div>
+        <p className={baseStyles["description"]}>
+          Leverage the power of Chat GPT in your journey to become the best MTG
+          player in the world!
+        </p>
       </div>
     </div>
   );
